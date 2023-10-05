1afa41afb4babf5b794825c6127bae1bbaee35b8
Majo <mjsilva890@misena.edu.co>
2023-10-05 17:24:12 -0500

    inicio-instructor

--- src/app/components/inicio-instructor/inicio-instructor.component.ts
+++ src/app/components/inicio-instructor/inicio-instructor.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio-instructor',
-  templateUrl: './inicio-instructor.component.html',
-  styleUrls: ['./inicio-instructor.component.css']
+  templateUrl: './inicio-instructor.component.html'
 })
 export class InicioInstructorComponent {
 
