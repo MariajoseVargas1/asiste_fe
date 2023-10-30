8a9a6ba835e21fdc90832c5514c681f7515fce89
Majo <mjsilva890@misena.edu.co>
2023-10-29 21:59:05 -0500

    registro de novedades--base

--- src/app/components/registro-novedad/registro-novedad.component.ts
+++ src/app/components/registro-novedad/registro-novedad.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './registro-novedad.component.html'
 })
 export class RegistroNovedadComponent {
+  parte1Visible = true;
+  parte2Visible = false;
 
+  mostrarParte2() {
+    this.parte1Visible = false;
+    this.parte2Visible = true;
+  }
 }
+
+
