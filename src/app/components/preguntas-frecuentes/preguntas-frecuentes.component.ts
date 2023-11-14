ca9692fba8e97c3d5df0188688c47516ff22ac29
Majo <mjsilva890@misena.edu.co>
2023-11-13 22:12:41 -0500

    preguntas frecuentespara revision

--- src/app/components/preguntas-frecuentes/preguntas-frecuentes.component.ts
+++ src/app/components/preguntas-frecuentes/preguntas-frecuentes.component.ts
@@ -4,8 +4,7 @@ import {CdkAccordionModule} from '@angular/cdk/accordion';
 @Component({
   selector: 'app-preguntas-frecuentes',
   templateUrl: './preguntas-frecuentes.component.html',
-  // standalone: true,
-  // imports: [CdkAccordionModule],
+
 })
 export class PreguntasFrecuentesComponent {
 
