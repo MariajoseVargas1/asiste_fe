8030b7146097e865fbd22a6d91c446f34fc824e8
Majo <mjsilva890@misena.edu.co>
2023-11-29 02:29:51 -0500

    Import a las clases y su registro en admin.py Tambien la creacion del super usuario Creando los modelos y agregandolos a admin

--- a_backend/admin.py
+++ a_backend/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import AsistenciaAprendiz
+from .models import AsistenciaAsistencia
+from .models import AsistenciaCoordinacion
+from .models import AsistenciaFicha
+from .models import AsistenciaFichaHorarioFicha
+from .models import AsistenciaHorarioPorDia
+from .models import AsistenciaInstructor
+from .models import AsistenciaNovedad
+from .models import AsistenciaPrograma
+
 
 # Register your models here.
+
+admin.site.register(AsistenciaAprendiz)
+admin.site.register(AsistenciaAsistencia)
+admin.site.register(AsistenciaCoordinacion)
+admin.site.register(AsistenciaFicha)
+admin.site.register(AsistenciaFichaHorarioFicha)
+admin.site.register(AsistenciaHorarioPorDia)
+admin.site.register(AsistenciaInstructor)
+admin.site.register(AsistenciaNovedad)
+admin.site.register(AsistenciaPrograma)
